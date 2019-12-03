5e0678ca21766b6e7ae8dd4e2fc6ccfcae1363e2
handi <poinsettiaaa@naver.com>
2019-12-04 00:40:52 +0900

    .

---
+++ app/src/main/java/com/example/handi3/Dog.kt
@@ -0,0 +1,3 @@
+package com.example.handi3
+
+class Dog (val breed: String, val gender: String, val age: String, val photo: String)
---
+++ app/src/main/java/com/example/handi3/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.handi3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var dogList = arrayListOf<Dog>(
+            Dog("Chow Chow", "Male", "4", "dog00"),
+            Dog("Breed Pomeranian", "Female", "1", "dog01"),
+            Dog("Golden Retriver", "Female", "3", "dog02"),
+            Dog("Yorkshire Terrier", "Male", "5", "dog03"),
+            Dog("Pug", "Male", "4", "dog04"),
+            Dog("Alaskan Malamute", "Male", "7", "dog05"),
+            Dog("Shih Tzu", "Female", "5", "dog06")
+        )
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+            val mAdapter = MainRvAdapter(this, dogList)
+            mRecyclerView.adapter = mAdapter
+
+            val lm = LinearLayoutManager(this)
+            mRecyclerView.layoutManager = lm
+            mRecyclerView.setHasFixedSize(true)
+        }
+    }
+}
